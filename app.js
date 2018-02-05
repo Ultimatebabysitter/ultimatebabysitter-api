b150870f0820f1515122fa2d95850e985278a055
Joseph Leon <joseph.leon.9@gmail.com>
2018-02-04 22:22:18 -0500

    Add error catching to middleware, add morgan and run requests through morgan.

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const userRoutes = require('./api/routes/users.js')
 
+app.use(morgan('dev'));
+
 app.use('/users', userRoutes);
+
+// error catching
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
 
 module.exports = app;
