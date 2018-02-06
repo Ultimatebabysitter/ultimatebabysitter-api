f839650d96a16bfd9e45ca8164e0cf172f700917
Joseph Leon <joseph.leon.9@gmail.com>
2018-02-05 20:42:36 -0500

    Add body-parser, add middleware for body parser, add middleware for cors.

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const userRoutes = require('./api/routes/users.js')
 
+// misc
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+// cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// routes
 app.use('/users', userRoutes);
 
 // error catching
