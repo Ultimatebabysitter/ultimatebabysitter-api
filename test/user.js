1a955f16f38510fb1b6947c8b1070eb121862541
Joseph Leon <joseph.leon.9@gmail.com>
2018-02-05 23:50:27 -0500

    Add assertion expect to check all keys in post body.

--- test/user.js
+++ test/user.js
@@ -2,6 +2,7 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+let expect = chai.expect;
 
 chai.use(chaiHttp);
 
@@ -19,13 +20,23 @@ describe('Users', () => {
   });
 
   // Test the /POST route
+  let userSample = {
+    "name": "Bertrand",
+    "age": 70,
+    "location": "123 mockingbird lane",
+    "type": "parent",
+    "pay": "10, 15",
+    "details": "Looking for a babysitter"
+  }
   describe('/POST users', () => {
     it('it should POST a user', (done) => {
       chai.request(server)
         .post('/users')
+        .send(userSample)
         .end((err, res) => {
           res.should.have.status(201);
           res.body.user.should.be.an('object');
+          expect(res.body.user).to.have.all.keys('name', 'age', 'location', 'type', 'pay', 'details');
           done();
         });
     });
