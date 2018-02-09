b20547361c6c304dd3f7f7d0dfe28c47ce821581
Joseph Leon <joseph.leon.9@gmail.com>
2018-02-08 23:11:37 -0500

    Add mongoose, add .env to gitignore.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const userRoutes = require('./api/routes/users.js')
 
