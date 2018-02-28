540ed72b8e0aaacf3a044d821675719d21b19bc3
Joseph Leon <joseph.leon.9@gmail.com>
2018-02-27 21:56:00 -0500

    Move server.js code into app.js, also add standard changes.

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 // routes
 const userRoutes = require('./api/routes/users.js')
 
 // connect to mongodb
-mongoose.connect(process.env.MONGODB_HOST);
+mongoose.connect(process.env.MONGODB_HOST)
 
 // misc
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
   }
-  next();
-});
+  next()
+})
 
 // routes
-app.use('/users', userRoutes);
+app.use('/users', userRoutes)
 
 // error catching
 app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-});
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
+  res.status(error.status || 500)
   res.json({
     error: {
       message: error.message
     }
   })
-});
+})
 
-module.exports = app;
+const http = require('http')
+// const app = require('./app')
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer(app)
+
+if (!module.parent) {
+  server.listen(port)
+}
+
+module.exports = app
