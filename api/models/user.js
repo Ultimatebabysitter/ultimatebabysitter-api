83d6515de332f2f5a3c42108e7338518839ca31d
Joseph Leon <joseph.leon.9@gmail.com>
2018-03-03 11:17:06 -0500

    Add validation to user model.

--- api/models/user.js
+++ api/models/user.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  first_name: String,
-  last_name: String,
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
   email: { type: String, required: true, index: { unique: true } },
-  age: Number,
-  location: String,
-  type: String,
+  age: { type: Number, required: true },
+  location: { type: String, required: true },
+  type: { type: String, required: true },
   pay: Number,
   details: String,
   date: { type: Date, default: Date.now },
   verification: String,
-  report: Boolean,
+  report: { type: Number },
   password: { type: String, required: true }
 })
 
