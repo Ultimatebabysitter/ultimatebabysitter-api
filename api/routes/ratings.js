d9a195efc14e842bc958ec11c4de6ae9e1f2775f
Joseph Leon <joseph.leon.9@gmail.com>
2018-03-08 21:03:25 -0500

    Add route for getting a users ratings.

--- api/routes/ratings.js
+++ api/routes/ratings.js
@@ -8,7 +8,7 @@ router.post('/', (req, res, next) => {
   const rating = Rating({
     _id: new mongoose.Types.ObjectId(),
     user: req.body.user,
-    rating: req.body.comment
+    rating: req.body.rating
   })
   rating
     .save()
@@ -24,3 +24,25 @@ router.post('/', (req, res, next) => {
       res.status(500).json({error: err})
     })
 })
+
+// get users average rating
+router.get('/:userId', (req, res, next) => {
+  const id = req.params.userId
+  Rating.findOne({ 'user': id })
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      console.log(req.params.userId);
+      if (doc) {
+        res.status(200).json(doc)
+      } else {
+        res.status(404).json({message: 'No valid entry found.'})
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    })
+})
+
+module.exports = router
