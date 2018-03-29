a11a6fbfdb94ae76abe8b2729e7f13d5e3eebfb0
josephleon <joseph.leon.9@gmail.com>
2018-03-29 11:53:04 -0400

    Add email matching regex, remove required for address2 field.

--- api/models/user.js
+++ api/models/user.js
@@ -3,10 +3,15 @@ const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
-  email: { type: String, required: true, index: { unique: true } },
+  email: {
+    type: String,
+    required: true,
+    index: { unique: true },
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  },
   age: { type: Number, min: 18, required: true },
   address1: {type: String, required: true },
-  address2: {type: String, required: true },
+  address2: {type: String},
   city: {type: String, required: true },
   state: {type: String, required: true },
   zip: {type: String, required: true },
