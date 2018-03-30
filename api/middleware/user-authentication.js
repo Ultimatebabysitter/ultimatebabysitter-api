8e0b5729cb5c5e099043455db73e27396b9ab178
josephleon <joseph.leon.9@gmail.com>
2018-03-30 10:23:08 -0400

    Run standard fix.

--- api/middleware/user-authentication.js
+++ api/middleware/user-authentication.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]
+    const token = req.headers.authorization.split(' ')[1]
     const decoded = jwt.verify(token, process.env.JWT_KEY)
     req.userData = decoded
     next()
-  } catch(error) {
+  } catch (error) {
     return res.status(401).json({message: 'auth failed'})
   }
 }
