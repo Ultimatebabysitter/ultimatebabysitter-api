ae35ea0fd94f2d81f340664a1ba19340a21c4be0
JosephLeon <joseph.leon.9@gmail.com>
2018-07-08 21:46:22 -0400

    Remove commented code.

--- api/middleware/sanitize-rating.js
+++ api/middleware/sanitize-rating.js
@@ -1,8 +1,5 @@
 module.exports = (req, res, next) => {
   try {
-    // req.body.rating = req.sanitize(req.body.rating)
-    // req.body.user = req.sanitize(req.body.user)
-    // req.body.reporting_user = req.sanitize(req.body.reporting_user)
     // sanitize all the fields
     const { body } = req;
     for (const [key, value] of Object.entries(body)) {
